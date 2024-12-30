2518e91dadc025bdaf1dec87da328b7a77d0fdac
Bal1oon <ws32159@naver.com>
2024-12-30 13:34:07 +0900

    fix: Update Student entity for ID generation

--- src/main/java/actions/entity/Student.java
+++ src/main/java/actions/entity/Student.java
@@ -2,6 +2,8 @@ package actions.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import lombok.Data;
 @Entity
 public class Student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	
